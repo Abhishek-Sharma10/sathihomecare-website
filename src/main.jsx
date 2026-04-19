8f8917c9a44936fdc7fd7aec60e5f1e6dbc4508f
Abs3496 <officialabhishek375@gmail.com>
2026-04-19 13:05:48 +0530

    Revert "feat: sync latest sathi frontend"
    This reverts commit a64b966654ef76b64128658fc6965dbef38da822.

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { CartProvider } from "./context/CartContext";
-import { AuthProvider } from "./context/AuthContext";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </AuthProvider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
--- vitest.config.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  test: {
-    environment: "jsdom",
-    globals: true
-  }
-};
